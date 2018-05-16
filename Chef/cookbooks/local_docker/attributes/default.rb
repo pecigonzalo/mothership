69c738ca1c82b96e45e5b784b821cb3c557a7efe
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2018-05-16 11:30:22 +0200

    Remove old chef code

--- Chef/cookbooks/local_docker/attributes/default.rb
+++
@@ -1,1 +0,0 @@
-default['zenmate_docker']['version'] = '1.13.0-0~debian-jessie'
--- Chef/cookbooks/local_media_server/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'local_media_server'
-maintainer       'NoName'
-maintainer_email 'NoEmail@NoName.com'
-license          'All rights reserved'
-description      'Installs/Configures local_media_server'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
